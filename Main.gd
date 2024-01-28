f7fc0c8fd4ad2ffbe0c053e4428644004754f79a
atesahmet0 <ahmetrizaates@hotmail.com>
2024-01-29 02:40:03 +0300

    New level implementation.
    Removed "Game" node from "Main" node and made it a scene named as level1.
    Now when game starts we instantiate level1 instead of making "Game" node visible.
    
    Removed unncessary code block from "NavigationRegion2D.gd"

--- Main.gd
+++ Main.gd
@@ -5,14 +5,14 @@ extends Node
 var flying_eye_dead_effect_scene = preload("res://Scenes/FlyingEyeDeadBlood.tscn")
 var flying_eye_scene = preload("res://Scenes/FlyingEye.tscn")
 var flying_eye_script = preload("res://Scenes/FlyingEye.gd")
+var level1 = preload("res://Scenes/Levels/level_1.tscn")
 
 var is_game_started = false
 func _ready():
 	if not is_game_started:
-		$Game.set_visible(false)
 		$MainMenu.set_visible(true)
-		$Game.find_child("Camera2D").position = $MainMenu.position + ($MainMenu.size / 2)
-		$Game.find_child("Camera2D").SHAKE_ON_WEAPON_FIRE = false
+		# $Game.find_child("Camera2D").position = $MainMenu.position + ($MainMenu.size / 2)
+		# $Game.find_child("Camera2D").SHAKE_ON_WEAPON_FIRE = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -20,12 +20,19 @@ func _process(delta):
 
 # Called when start game clicked.
 func _on_main_menu_start_clicked():
+	# This shouldn't happen. Just in case.
+	if is_game_started:
+		return
+	
 	is_game_started = true
-	$Game.set_visible(true)
+	# $Game.set_visible(true)
 	$MainMenu.set_visible(false)
+	var new_level = level1.instantiate()
+	add_child(new_level)
+	
 	# Changing cameras y value to where it belongs.
-	$Game.find_child("Camera2D").position.y = -402 
-	$Game.find_child("Camera2D").SHAKE_ON_WEAPON_FIRE = true
+	# $Game.find_child("Camera2D").position.y = -402 
+	# $Game.find_child("Camera2D").SHAKE_ON_WEAPON_FIRE = true
 
 
 
--- NavigationRegion2D.gd
+++ NavigationRegion2D.gd
@@ -4,23 +4,7 @@ extends NavigationRegion2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var rect = tiles.get_used_rect() #Gets entire region of tilemap
-	var used = tiles.get_used_cells(0) #Get the filled in tiles(Mine are in layer 0)
-	var all_cells = []
-	#interate over all possible tile locations
-	for x in range(int(rect.position.x), int(rect.end.x)):
-		for y in range(int(rect.position.y), int(rect.end.y)):
-			#Combine all possible positions into array
-			all_cells.append(Vector2(x, y))
-	#Now we want to remove all used cells
-	#This leaves us with just the empty areas that we want navigation in
-	for cell in used:
-		all_cells.erase(Vector2(cell))
-	#Now we set the open cells to a clear tile with no collision
-	#Make sure navigation is set up on these tiles
-	for cell in all_cells:
-		tiles.set_cell(1, Vector2i(cell), 1, Vector2i(0, 0), 0) 
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
