cc78896ab294c6b468f0a5dc317d85c4586d735b
atesahmet0 <ahmetrizaates@hotmail.com>
2024-01-31 23:21:08 +0300

    Camera shake fix and Shotgun shader improvement

--- Camera2D.gd
+++ Camera2D.gd
@@ -9,14 +9,13 @@ signal moved(camera: Camera2D)
 # Do not change this on runtime. Results will be unpredictable
 @export var SHAKE_ON_WEAPON_FIRE: bool = true
 @export var DECAY = 4  # How quickly the shaking stops [0, 1].
-@export var MAX_OFFSET = Vector2(50, 10)  # Maximum hor/ver shake in pixels.
+@export var MAX_OFFSET = Vector2(5, 10)  # Maximum hor/ver shake in pixels.
 @export var MAX_ROLL: float = 0.5  # Maximum rotation in radians (use sparingly).
-@export var TRAUMA = 1.75
+@export var TRAUMA = 1.60
 # Exponent
 @export var TRAUMA_POWER = 2
-# Called when the node enters the scene tree for the first time.
 
-var trauma = 1.0
+var trauma = 0
 
 func _ready():
 	# I don't know what is this. I guess it is useless.
@@ -29,8 +28,6 @@ func _process(delta):
 		position.x = playerNode2D.position.x
 		moved.emit(self)
 		align()
-		
-	# Y axis movement is disabled for a while.
 	
 	if trauma and SHAKE_ON_WEAPON_FIRE:
 		trauma = max(trauma - DECAY * delta, 0)
@@ -44,6 +41,6 @@ func shake():
 	offset.y = MAX_OFFSET.y * amount * randf_range(-1, 1)
 
 
-func _on_shotgun_weapon_fired():
+func _on_shotgun_weapon_fired(current_ammo: int):
 	trauma = TRAUMA
 
